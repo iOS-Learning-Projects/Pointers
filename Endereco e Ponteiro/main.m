cf95757c87c95976874b7570412ca12032135610
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2018-02-12 14:04:45 -0200

    just add

--- Endereco e Ponteiro/main.m
+++ Endereco e Ponteiro/main.m
@@ -8,10 +8,37 @@
 
 #import <Foundation/Foundation.h>
 
+void dumbSort(int *ptr, int len) {
+    for (int i = 0; i < (len - 1); i++) {
+        if (ptr[i] > ptr[i+1]) {
+            int aux = ptr[i];
+            ptr[i] = ptr[i+1];
+            ptr[i+1] = aux;
+            i = -1;
+        }
+    }
+}
+
+void printArray(int *arr, int len) {
+    for (int i = 0; i < len; i++) {
+        printf("%d ", arr[i]);
+    }
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int *arrayPtr = NULL;
+        int len = 5; // Array size
+
+        arrayPtr = (int *) malloc(len * sizeof(int));
+
+        for (int i = 0; i < len; i++) {
+            arrayPtr[i] = arc4random_uniform(100);
+        }
+
+        dumbSort(arrayPtr, len);
+        printArray(arrayPtr, len);
+        free(arrayPtr);
     }
     return 0;
 }
